1493511756e055c8c23c82f03651b5ec1b9b8358
Bharav Tumati <bhargav.tumati15@gmail.com>
2024-10-21 18:53:40 +0530

    dsa

---
+++ DSA/maxArea.py
@@ -0,0 +1,27 @@
+from typing import List
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left = 0
+        right = len(height) - 1
+        max_area = 0
+        
+        while left < right:
+            # Calculate the area
+            width = right - left
+            current_height = min(height[left], height[right])
+            current_area = width * current_height
+            max_area = max(max_area, current_area)
+            
+            # Move the pointer pointing to the shorter line
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        
+        return max_area
+
+# Example usage
+solution = Solution()
+height = [1,8,6,2,5,4,8,3,7]
+print(solution.maxArea(height))  # Output: 49
